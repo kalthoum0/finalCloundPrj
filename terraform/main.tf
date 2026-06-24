ffc413ff063ae88ef56238f03475194d932ad777
Kalthoum AL-Atrash <kalthoum_4853@limu.edu.ly>
2026-06-24 23:24:44 +0200

    feat(terraform): add infrastructure configuration and gitignore

---
+++ terraform/main.tf
@@ -0,0 +1,62 @@
+# Resource Group
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    Project     = "Final"
+    StudentName = var.student_name
+  }
+}
+
+# Azure Container Registry
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku           = "Basic"
+  admin_enabled = false
+
+  tags = {
+    Project     = "Final"
+    StudentName = var.student_name
+  }
+}
+
+# Azure Kubernetes Service
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  dns_prefix = "KR-k8s"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Project     = "Final"
+    StudentName = var.student_name
+  }
+}
+
+# AKS -> ACR Integration
+# Allows AKS to pull images automatically without secrets
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
