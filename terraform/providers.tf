aba0ae0a438e350941507562d5c1a0907bf6c687
Kalthoum AL-Atrash <kalthoum_4853@limu.edu.ly>
2026-06-25 13:48:12 +0200

    chore(infra): migrate terraform state to azure remote backend storage

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "final_kalthoum_rawan_prj"
+    storage_account_name = "kalthoumrawantfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
